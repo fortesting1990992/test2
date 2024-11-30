9c9d304b26a0a51448ea7d97290a72a994168c4f
men <hello@gmail.com>
2024-11-30 19:50:02 +0500

    first

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] ars) throws IOException {
+
+        System.out.println("salom");
+        System.out.println("salom");
+
+    }
+}
